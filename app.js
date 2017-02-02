795c5df72cfe192b3e0c27267f94887dafd67384
tron04 <tron04@gmail.com>
2017-02-01 22:37:21 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var http = require('http')
+
+var server = http.createServer(function (request, response) {
+  response.end("Hello World");
+})
+
+server.listen(3000)
---
+++ ecosystem.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : "node-example",
+      script    : "app.js",
+      env_production : {
+        NODE_ENV: "production"
+      },
+      env_staging : {
+        NODE_ENV: "staging"
+      }
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : "deploy",
+      host : "138.197.222.247",
+      ref  : "origin/master",
+      repo : "https://cf06131c137fae597af6720f6c4f05996389fe2b@github.com/ericsproles/node-example.git",
+      path : "~/dev/learncode/node-example",
+      "post-deploy" : "nvm install && npm install && pm2 startOrRestart ecosystem.json --env production"
+    },
+    // staging : {
+    //   user : "node",
+    //   host : "212.83.163.1",
+    //   ref  : "origin/master",
+    //   repo : "git@github.com:repo.git",
+    //   path : "/var/www/development",
+    //   "post-deploy" : "npm install && pm2 startOrRestart ecosystem.json --env dev",
+    //   env  : {
+    //     NODE_ENV: "dev"
+    //   }
+    // }
+  }
+}
